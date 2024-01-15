6d4dd4db9857a8b5d55e0d7c3da8b41363b9ab79
saqibahmed13 <saqiba0484@gmail.com>
2024-01-15 19:28:14 +0530

    added

---
+++ JS/objects.js
@@ -0,0 +1,16 @@
+// Objects should be use when elements are of string 
+// array should be use when elements are of numbers 
+
+
+const person = {
+    name:"saqib",
+    lname:"ahmed",
+    age: 24,
+    gender:"male"
+};
+
+console.log(person.name);
+
+person.name = "sagheer";
+// can modify like this 
+console.log(person.name);
