d0c637a6edfeccb055cb4e79fe55769670b44d72
saqibahmed13 <saqiba0484@gmail.com>
2024-01-21 13:07:10 +0530

    added

--- JS/DOM.js
+++ JS/DOM.js
@@ -8,3 +8,7 @@ console.log("hello");
 window.console.log("hello2"); // to use this link the js to html and see in the console
 
 // DOM :  HTML can be accessed in javascript which is known as document and these document will be appear in window object (browser)
+
+//document.querySelector("idvalue or class or tags (p) ") : this will return the value of 1st elemnent of class or tag or id 
+
+//document.querySelectorAll : this will return all the values of tags or ids or classes 
